f30def642f1559e793ea5404ea29b17de424da97
Jeffrey D. Gordon <jeff@jdgwf.com>
2020-01-28 08:14:11 -0600

    Removed some server cruft

--- linecount.sh
+++
@@ -1,7 +0,0 @@
-echo "TS Files:"
-find . -type f -name '*.tsx' -not -path "./node_modules/*" | xargs wc -l
-echo "TSX Files:"
-find . -type f -name '*.ts' -not -path "./node_modules/*" | xargs wc -l
-
-echo "TSX and TS Files:"
-find . -type f \( -name "*.tsx" -o -name "*.ts" \) -not -path "./node_modules/*" | xargs wc -l
